5e7eff285b0f112b2fccb9937b1d495d2891b90b
PhuHuy123 <nguyenphuhuy180920@gmail.com>
2022-04-03 15:33:32 +0700

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,5 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
 end
